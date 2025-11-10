55487392212f57f1a62aac3e2ce329f3dd59739c
Saucesamba <arhipartyom@yandex.ru>
2025-11-10 11:19:22 +0300

    part of models

--- questions/models.py
+++ questions/models.py
@@ -1,25 +1,34 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
 
 class Tag(models.Model):
     title = models.CharField(max_length = 20)
 
-class QuestionAnswer(models.Model):
-    text = models.TextField(max_length=500)
-    is_correct = models.BooleanField(False)
-    like_count = models.IntegerField()
 
 class Question(models.Model):
     title = models.CharField(max_length=60)
     question_text = models.TextField(max_length=1000)
     tags = models.ManyToManyField(Tag)
-    is_hot = models.BooleanField(True)
-    answers = models.ForeignKey(QuestionAnswer, on_delete=models.CASCADE)
-    like_count = models.IntegerField()
+    is_hot = models.BooleanField(True) 
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
+
+
+class QuestionAnswer(models.Model):
+    text = models.TextField(max_length=500)
+    is_correct = models.BooleanField(False)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
 
     
+class AnswerLike(models.Model):
+    status = models.BooleanField(True) # лайк или дизлайк True - лайк, False - дизлайк
+    like = models.ForeignKey(QuestionAnswer, on_delete=models.CASCADE)
 
 
-
+class QuestionLike(models.model):
+    status = models.BooleanField(True) # лайк или дизлайк True - лайк, False - дизлайк
+    like = models.ForeignKey(Question, on_delete=models.CASCADE)
 

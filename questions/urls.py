0be33c0e2c90e6f4ba6f06e76b01ab8e3615d58a
Saucesamba <arhipartyom@yandex.ru>
2025-12-24 00:37:32 +0300

    all likes functional

--- questions/urls.py
+++ questions/urls.py
@@ -7,6 +7,11 @@ urlpatterns = [
     path('hot/', HotQuestionsView.as_view(), name = "hot"),
     path('tag/<str:tag>/', TagFilteredQuestionsView.as_view(),name = "tag"),
     path('question/<int:pk>/', OneQuestionView.as_view(), name = "question_details"),
-    path('ask/', NewQuestionView.as_view(), name = "ask")
+    path('ask/', NewQuestionView.as_view(), name = "ask"),
+
+    path('api/question/<int:pk>/toggle_like',QuestionLikeAPIView.as_view(), name = 'question_like'),
+    path('api/answer/<int:pk>/toggle_like', AnswerLikeAPIView.as_view(), name = 'answer_like'),
+    path('api/question/<int:pk>/correct_answer', AnswerCorrectAPIView.as_view(), name = 'correct')
+
 ]
 
